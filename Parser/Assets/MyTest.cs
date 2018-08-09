f94b579c221e95eb5c1c9fb3b7f4ca06a678a5e1
aka_3D-PC <aka@aviareal.com>
2018-08-10 01:46:52 +0300

    Внесен проект Parser - добавленн тест с возможностью ввода произвольного выражения

---
+++ Parser/Assets/MyTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BooleanLogicParser;
+
+public class MyTest : MonoBehaviour {
+
+    [SerializeField]
+    string myExpression;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            print(myExpression + " = " + CanParseSingleToken(myExpression));
+        }
+
+	}
+
+    bool CanParseSingleToken(string expression)
+    {
+        var tokens = new Tokenizer(expression).Tokenize();
+        var parser = new Parser(tokens);
+        return parser.Parse();
+    }
+}
