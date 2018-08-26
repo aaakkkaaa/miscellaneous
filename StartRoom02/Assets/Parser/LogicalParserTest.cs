bb9b7707626142cc93fe903be7739d750a5e081d
fsamin <fsamin@yandex.ru>
2018-08-26 07:20:26 +0300

    Изменения в scrHerder и пр.

---
+++ StartRoom02/Assets/Parser/LogicalParserTest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BooleanLogicParser;
+
+public class LogicalParserTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        print(CanParseSingleToken("true")); // ExpectedResult = true)
+        //print(CanParseSingleToken(")")); // ExpectedException = (typeof(Exception)))
+        //print(CanParseSingleToken("az")); // ExpectedException = (typeof(Exception)))
+        //print(CanParseSingleToken("")); // ExpectedException = (typeof(Exception)))
+        //print(CanParseSingleToken("()")); // ExpectedException = typeof(Exception))
+        //print(CanParseSingleToken("true and")); // ExpectedException = typeof(Exception))
+        print(CanParseSingleToken("false")); // ExpectedResult = false)
+        print(CanParseSingleToken("true ")); // ExpectedResult = true)
+        print(CanParseSingleToken("false ")); // ExpectedResult = false)
+        print(CanParseSingleToken(" true")); // ExpectedResult = true)
+        print(CanParseSingleToken(" false")); // ExpectedResult = false)
+        print(CanParseSingleToken(" true ")); // ExpectedResult = true)
+        print(CanParseSingleToken(" false ")); // ExpectedResult = false)
+        print(CanParseSingleToken("(false)")); // ExpectedResult = false)
+        print(CanParseSingleToken("(true)")); // ExpectedResult = true)
+        print(CanParseSingleToken("true and false")); // ExpectedResult = false)
+        print(CanParseSingleToken("false and true")); // ExpectedResult = false)
+        print(CanParseSingleToken("false and false")); // ExpectedResult = false)
+        print(CanParseSingleToken("true and true")); // ExpectedResult = true)
+        print(CanParseSingleToken("!true")); // ExpectedResult = false)
+        print(CanParseSingleToken("!(true)")); // ExpectedResult = false)
+        //print(CanParseSingleToken("!(true")); // ExpectedException = typeof(Exception))
+        print(CanParseSingleToken("!(!(true))")); // ExpectedResult = true)
+        print(CanParseSingleToken("!false")); // ExpectedResult = true)
+        print(CanParseSingleToken("!(false)")); // ExpectedResult = true)
+        print(CanParseSingleToken("(!(false)) and (!(true))")); // ExpectedResult = false)
+        print(CanParseSingleToken("!((!(false)) and (!(true)))")); // ExpectedResult = true)
+        print(CanParseSingleToken("!false and !true")); // ExpectedResult = false)
+        print(CanParseSingleToken("false and true and true")); // ExpectedResult = false)
+        print(CanParseSingleToken("false or true or false")); // ExpectedResult = true)
+    }
+
+    public bool CanParseSingleToken(string expression)
+    {
+        var tokens = new Tokenizer(expression).Tokenize();
+        var parser = new Parser(tokens);
+        return parser.Parse();
+    }
+}
